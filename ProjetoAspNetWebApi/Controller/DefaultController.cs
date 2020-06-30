ff774155a8b34c4b3f74b51e0f541c220f3e49e8
jrguedelha <jrguedelha@gmail.com>
2020-06-30 15:25:31 -0300

    Criado DefaultController e o metodo GetDataHoraServer

---
+++ ProjetoAspNetWebApi/Controller/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProjetoAspNetWebApi.Controller
+{
+    [RoutePrefix("api/meuprojeto")]
+    public class DefaultController : ApiController
+    {
+        [HttpGet]
+        [Route("datahora/get")]
+        public HttpResponseMessage GetDataHoraServer()
+        {
+            try
+            {
+                var dataHora = DateTime.Now.ToString("dd/MM/yy HH:mm");
+
+                return Request.CreateResponse(HttpStatusCode.OK, dataHora);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
